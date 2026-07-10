a78b72bf9d53ad471e550d52ced8ffc794f0d848
meettroublezzz <1175113305@qq.com>
2026-07-10 17:27:56 +0800

    Nothing

---
+++ sparrow-cloud-backend/SparrowCloud.Models/Union/UnionMount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparrowCloud.Models.Union
+{
+    /// <summary>
+    /// 挂载点
+    /// </summary>
+    public class UnionMount : EntityIncr<int>
+    {
+    }
+}
